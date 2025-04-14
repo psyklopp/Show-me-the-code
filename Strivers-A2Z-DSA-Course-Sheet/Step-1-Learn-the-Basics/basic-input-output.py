1aa4cddc3c03975518141f987e49378bcc88e8d4
Aditya Bhardwaj <aditbhardwj@gmail.com>
2025-04-14 17:04:57 +0200

    started strivers A2Z sheet

---
+++ Strivers-A2Z-DSA-Course-Sheet/Step-1-Learn-the-Basics/basic-input-output.py
@@ -0,0 +1,20 @@
+#Basic input and output
+#Language: Python
+
+
+# Getting the input from the user
+number = input("What is your favorite number: ")
+# print it
+print("Your favorite numner is:", number)
+
+# Let's see what is the data type
+print("The data type of the above number is: ",type(number))
+
+# Let's convert it to the int type
+number = int(number)
+print("Convert to int data type")
+
+# After conversion let's see the data type
+print("The data type of the above number is: ",type(number))
+
+## Next part will be taking multiple inputs from the user
